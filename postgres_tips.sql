d0e4e07ed3f1d7b85d211d113b85edf50e0f16ff
Shahboz <sharafzodashahboz@gmail.com>
2023-11-17 13:50:41 +0500

    some tips for adding columns

--- postgres_tips.sql
+++ postgres_tips.sql
@@ -4,3 +4,22 @@
 SELECT table_name 
 FROM information_schema.tables
 WHERE table_schema = 'test' and table_type = 'BASE TABLE';
+
+-- if you want to remove a column from table you can do like this
+
+ALTER TABLE [ptable_name]
+DROP COLUMN [column_name];
+
+
+-- if you want to add a column to all the tables of specific schema you can do 
+
+DO $$ 
+DECLARE 
+    current_table text;
+BEGIN 
+    FOR current_table IN (SELECT table_name FROM information_schema.tables WHERE table_schema = 'tjk11' AND table_type = 'BASE TABLE') 
+    LOOP
+        EXECUTE 'ALTER TABLE test.' || current_table || ' ADD COLUMN a291_f10_multi_search varchar[]';
+    END LOOP;
+END $$;
+
