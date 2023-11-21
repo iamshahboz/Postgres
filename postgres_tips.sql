ba4dc7d8ac33a989dbce5c686efc646c7f5e2644
Shahboz <sharafzodashahboz@gmail.com>
2023-11-21 16:39:37 +0500

    tips added

--- postgres_tips.sql
+++ postgres_tips.sql
@@ -17,9 +17,9 @@ DO $$
 DECLARE 
     current_table text;
 BEGIN 
-    FOR current_table IN (SELECT table_name FROM information_schema.tables WHERE table_schema = 'tjk11' AND table_type = 'BASE TABLE') 
+    FOR current_table IN (SELECT table_name FROM information_schema.tables WHERE table_schema = 'test' AND table_type = 'BASE TABLE') 
     LOOP
-        EXECUTE 'ALTER TABLE test.' || current_table || ' ADD COLUMN a291_f10_multi_search varchar[]';
+        EXECUTE 'ALTER TABLE test.' || current_table || ' ADD COLUMN age integer';
     END LOOP;
 END $$;
 
@@ -30,7 +30,7 @@ DECLARE
     current_table text;
 BEGIN 
     FOR current_table IN (SELECT table_name FROM information_schema.tables 
-              WHERE table_schema = 'tjk11' AND 
+              WHERE table_schema = 'test' AND 
               table_type = 'BASE TABLE' and 
               (table_name like 'pr_f1_sr%' or table_name like 'f1%'
 			  or table_name like 'f2%'
@@ -45,41 +45,9 @@ BEGIN
 			  or table_name like 'resh_sr%')) order by table_name 
     LOOP
 		raise notice 'tbl %', current_table;
-        EXECUTE 'ALTER TABLE tjk11.' || current_table || ' ADD COLUMN c31_f11_multi_search varchar[]';
+        EXECUTE 'ALTER TABLE tjk11.' || current_table || ' ADD COLUMN age integer';
     END LOOP;
 END $$;
-
---for updating you can do
-
-DO $$ 
-DECLARE 
-    current_table text;
-BEGIN 
-    FOR current_table IN (SELECT table_name FROM information_schema.tables 
-              WHERE table_schema = 'tjk11' AND 
-              table_type = 'BASE TABLE' and 
-              (table_name like 'pr_f1_sr%' or table_name like 'f1%'
-			  or table_name like 'pr_f1_ppl_sr%'
-			  or table_name like 'f1%')) order by table_name 
-    LOOP
-		raise notice 'tbl %', current_table;
-         EXECUTE 'UPDATE tjk11.' || current_table || '
-        SET 
-        c291_f10_multi_search = tjk11.cust_str_to_arr(tjk11.multi_search_rez_os_mesta_pro(c291_f10::int)),
-        d291_f10_multi_search = tjk11.cust_str_to_arr(tjk11.multi_search_rez_os_mesta_pro(d291_f10::int)),
-		a31_f11_multi_search = tjk11.cust_str_to_arr(tjk11.multi_search_prim_krim_sresv(a31_f11::int)),
-		b31_f11_multi_search = tjk11.cust_str_to_arr(tjk11.multi_search_prim_krim_sresv(a31_f11::int)),
-		c31_f11_multi_search = tjk11.cust_str_to_arr(tjk11.multi_search_prim_krim_sresv(c31_f11::int));';
-
-    END LOOP;
-END $$;
-
--- in order to list the tables which has the specific column you can do
-
-SELECT table_name
-FROM information_schema.columns
-WHERE table_schema = 'tjk11' -- replace with your schema name
-  AND column_name = 'c31_f11'; -- replace with your column name
 
 
 
@@ -94,7 +62,7 @@ FROM
 JOIN
     pg_namespace ON pg_proc.pronamespace = pg_namespace.oid
 WHERE
-    prosrc ILIKE '%a291_f10%'
+    prosrc ILIKE '%age%'
 ORDER BY
     function_name;
 
