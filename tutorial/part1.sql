5373e58fc6ef85eea213bd894b83a210e2e2e9bd
Shahboz <sharafzodashahboz@gmail.com>
2024-06-12 01:12:01 -0700

    creating table and inserting data

--- tutorial/part1.sql
+++ tutorial/part1.sql
@@ -22,8 +22,36 @@ CREATE TABLE table_name (
     Column name + data type + constraints if any
 )
 
-time 43:14
-
  */
+
+ CREATE TABLE person(
+    id SERIAL NOT NULL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    gender VARCHAR(7) NOT NULL,
+    date_of_birth DATE NOT NULL
+    email VARCHAR(150)
+ );
+
+ /* 
+ \d person - this command will describe the table we created
+ */
+
+ /* inserting data to the table 
+ note that you should never use double quotes
+  */
+
+  INSERT INTO person(
+    first_name,
+    last_name,
+    gender,
+    date_of_birth,
+    email 
+  ) VALUES ('Anna','Tamanova','FEMELE', date '1992-09-12','annatam@gmail.com');
+ 
+
+ 
+ 
+
 
 
