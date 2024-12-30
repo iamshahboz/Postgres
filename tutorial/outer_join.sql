1b866b3303701252277133437ac44e1985c8c27d
shahboz <sharafzodashahboz@gmail.com>
2024-12-30 13:52:04 +0500

    alternative command added

--- tutorial/outer_join.sql
+++ tutorial/outer_join.sql
@@ -9,3 +9,9 @@ it doesn't matter if there is no match
 SELECT * FROM person LEFT OUTER JOIN car ON car.id = person.car_id
 UNION 
 SELECT * FROM person RIGHT OUTER JOIN car on car.id = person.car_id;
+
+
+/* or */
+
+SELECT * FROM person FULL OUTER JOIN car on car.id = person.car_id;
+
