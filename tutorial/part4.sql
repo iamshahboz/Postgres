6ddcb268ce68682a05932bb10deff3f8045044a7
Shahboz <sharafzodashahboz@gmail.com>
2025-01-06 02:27:43 +0500

    alias

---
+++ tutorial/part4.sql
@@ -0,0 +1,7 @@
+--alias. 
+/*
+Imagine you have a query and as the result you have two tables with the same names which is not good.
+What you can do is, to have an alias 
+
+*/
+SELECT first_name as "customer_name" from person;
